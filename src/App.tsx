401137f20946b95f3596e64dfe2991cb455c2a46
Szymon Kruk <szymonkruk813@gmail.com>
2023-06-15 18:49:27 +0200

    Fix styles and add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AboutMe from './components/AboutMe/AboutMe';
 import Skills from './components/Skills/Skills';
 import Navbar from './components/Navbar/Navbar';
 import Experience from './components/Experience/Experience';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AboutMe />
       <Skills />
       <Experience />
+      <Projects />
     </>
   );
 }
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h1 className="heading-of-section">PROJECTS</h1>
+    </section>
+  );
+};
+
+export default Projects;
